addba4df03753bee8c577e6d89ef5e33f6871100
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-22 23:38:13 +0000

    more options to runscript
    git-svn-id: https://svn.generation.no/neatzsche@84 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- trunk/cpp/run-go.sh
+++ trunk/cpp/run-go.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-let "ANODES=$3"
-cat "templates/distconfig-go.txt.template" | sed -e "s/GENOMES/${1}/g" -e "s/NODENUM/${2}/g" -e "s/GENS/${4}/g" -e "s!SETTINGFILE!${5}!g" > "distconfig-go.txt"
-cat "templates/pbs_go.sh.template" | sed -e "s/NODENUM/${ANODES}/g" -e "s/QUEUE/${6}/" > pbs_go.sh
+let "ANODES=$2-1"
+echo $ANODES
+cat "templates/distconfig-go.txt.template" | sed -e "s/GENOMES/${1}/g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${3}/g" -e "s/PGENS/${4}/g" -e "s/RUNS/${5}/g" -e "s!SETTINGFILE!${6}!g" > "distconfig-go.txt"
+cat "templates/pbs_go.sh.template" | sed -e "s/NODENUM/${2}/g" -e "s/QUEUE/${7}/" > pbs_go.sh
 chmod a+x pbs_go.sh
-if [[ ${7} != "0" ]]
+if [[ ${8} != "0" ]]
 then
-    if [[ ${7} = "1" ]]
+    if [[ ${8} = "1" ]]
     then
 	qsub -I pbs_go.sh
     fi
