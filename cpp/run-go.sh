76847c368e44d4b65385499dcc56f6567b3ad9fc
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-23 00:07:25 +0000

    more options to runscript
    git-svn-id: https://svn.generation.no/neatzsche/trunk@85 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- cpp/run-go.sh
+++ cpp/run-go.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 let "ANODES=$2-1"
 echo $ANODES
-cat "templates/distconfig-go.txt.template" | sed -e "s/GENOMES/${1}/g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${3}/g" -e "s/PGENS/${4}/g" -e "s/RUNS/${5}/g" -e "s!SETTINGFILE!${6}!g" > "distconfig-go.txt"
+cat "templates/distconfig-go.txt.template" | sed -e "s/GENOMES/${1}/g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${3}/g" -e "s/PENS/${4}/g" -e "s/RUNS/${5}/g" -e "s!SETTINGFILE!${6}!g" > "distconfig-go.txt"
 cat "templates/pbs_go.sh.template" | sed -e "s/NODENUM/${2}/g" -e "s/QUEUE/${7}/" > pbs_go.sh
 chmod a+x pbs_go.sh
 if [[ ${8} != "0" ]]
