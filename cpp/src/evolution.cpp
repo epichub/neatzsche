1d02d287c9a8dc67d9ebe506ee665e7e99f800e2
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-24 13:25:11 +0000

    ...
    git-svn-id: https://svn.generation.no/neatzsche/trunk@92 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- cpp/src/evolution.cpp
+++ cpp/src/evolution.cpp
@@ -345,7 +345,8 @@ void Species::reproduce()
     double testfn = p->fe->f(members->at(0));
     if(testfn!=members->at(0)->getOrigFitness()){
       cerr << "testfn var: " << testfn << " exiting.. " << endl;
-      cerr << "test var: " << (testfn!=members->at(0)->getOrigFitness()) ? "true" : "false" << endl;
+      cerr << "test var: ";
+      cerr << (testfn!=members->at(0)->getOrigFitness()) ? "true" : "false" << endl;
       exit(1);
     }
     members->at(0)->decClones();
