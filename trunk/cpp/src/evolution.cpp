c74338e4b1fd3d142eaec6a6852eaf77e9e754b7
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-25 06:06:41 +0000

    heh i misunderstood my own semantics
    git-svn-id: https://svn.generation.no/neatzsche@99 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- trunk/cpp/src/evolution.cpp
+++ trunk/cpp/src/evolution.cpp
@@ -343,8 +343,7 @@ void Species::reproduce()
 	 <<  " testf: " << p->fe->f(members->at(0))
 	 << "  new fitness: " << p->fe->f(newgeneration->at(0)) << endl;
     double testfn = p->fe->f(members->at(0));
-    if(myequal(testfn,members->at(0)->getOrigFitness())){
-      
+    if(!myequal(testfn,members->at(0)->getOrigFitness())){
       cerr.precision(99);
       cerr << "of var: "<< members->at(0)->getOrigFitness()
 	   <<" testfn var: " << testfn << " exiting.. " << endl;
