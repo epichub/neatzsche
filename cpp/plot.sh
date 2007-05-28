0cb47f6e208bad30fc20b3a83bc449d8d4dc52f7
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-28 13:51:30 +0000

    jada
    git-svn-id: https://svn.generation.no/neatzsche/trunk@160 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- cpp/plot.sh
+++ cpp/plot.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-cat "templates/plot.plt.template" | sed -e "s!current.graph!${1}!g" -e "s!current.png!${1}.png!g" > "${1}.plt"
-gnuplot "${1}.plt"
+if [[ $2 = 0 ]]
+then
+    cat "templates/plot.plt.template" | sed -e "s!current.graph!${1}!g" -e "s!current.png!${1}.png!g" > "${1}.plt"
+    gnuplot "${1}.plt"
+else
+    cat "templates/plotpersist.plt.template" | sed -e "s!current.graph!${1}!g" > "${1}.plt"
+    gnuplot "${1}.plt" &
+fi
+
 rm "${1}.plt"
