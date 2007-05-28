1661f0342f85ef6ce61cb6de204b6068e07a06a3
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-28 12:22:26 +0000

    ..
    git-svn-id: https://svn.generation.no/neatzsche/trunk@155 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- ruby/observer.rb
+++ ruby/observer.rb
@@ -48,7 +48,7 @@ end
 @neatrun = NEATRun.new
 print "input number of hours: "
 @hours = gets
-@hours = @hours[0..(@hours.size-2)]
+@hours = @hours[0..(@hours.size-2)].to_i
 @hours = @hours-48
 puts "you inputted: \"" + @hours.to_s + "\""
 @neatrun.makelist(@hours)
