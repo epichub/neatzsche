c9548b1694e258bf2cbc405fb84300e15408a33f
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2007-05-31 10:50:39 +0000

    new iswitchb stuff
    git-svn-id: https://svn.generation.no/neatzsche@187 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- trunk/cpp/plotbest.sh
+++ trunk/cpp/plotbest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat "templates/plotbest.plt.template" | sed -e "s!current.graph!${1}!g" -e "s!current.png!${1}-best.png!g" > "${1}.plt"
+cat "templates/plotbest.plt.template" | sed -e "s!current.graph!${1}!g" -e "s!current.pdf!${1}-best.pdf!g" > "${1}.plt"
 gnuplot "${1}.plt"
 rm "${1}.plt"
