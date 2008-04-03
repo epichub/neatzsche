a28055d17c4a821d81ac22e34d53a3254ee33d35
wakko <wakko@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2008-04-03 18:32:49 +0000

    meh, treigt igjen...
    git-svn-id: https://svn.generation.no/neatzsche/trunk@246 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- cpp/src/lightsim/lightvector.h
+++ cpp/src/lightsim/lightvector.h
@@ -12,7 +12,7 @@ private:
   LSC *lsc;
   nVector *nvec;
 public:
-  Lightvector(Lightsource* ls, LSC* lsc) { this->ls=ls; this->lsc=lsc; nvec=new nVector(lsc->getX(),lsc->getY(),ls->getX(),ls->getY()); }
+  Lightvector(LSC* lsc, Lightsource* ls) { this->ls=ls; this->lsc=lsc; nvec=new nVector(lsc->getX(),lsc->getY(),ls->getX(),ls->getY()); }
   ~Lightvector() {
     delete nvec;
   }
