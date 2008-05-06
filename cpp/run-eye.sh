18ee29d983bf4a2389fd09ee043fbd6306cc7cdf
wakko <wakko@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2008-05-06 11:48:00 +0000

    fix
    git-svn-id: https://svn.generation.no/neatzsche/trunk@288 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- cpp/run-eye.sh
+++ cpp/run-eye.sh
@@ -4,14 +4,13 @@ POPOP=$1
 let "ANODES=$2-1"
 NODES=$2
 GENERATIONS=$3
-PUREGENERATIONS=$4
-RUNS=$5
-SETTINGS=$6
-QUEUE=$7
-INTER=$8
+RUNS=$4
+SETTINGS=$5
+QUEUE=$6
+INTER=$7
 
 echo $ANODES
-cat "templates/distconfig-eye.txt.template" | sed -e "s!POP!${POPOP}!g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${GENERATIONS}/g" -e "s/PENS/${PUREGENERATIONS}/g" -e "s/RUNS/${RUNS}/g" -e "s!SETTINGFILE!${SETTINGS}!g" > "distconfig-eye.txt"
+cat "templates/distconfig-eye.txt.template" | sed -e "s!POP!${POPOP}!g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${GENERATIONS}/g" -e "s/RUNS/${RUNS}/g" -e "s!SETTINGFILE!${SETTINGS}!g" > "distconfig-eye.txt"
 cat "templates/pbs_eye.sh.template" | sed -e "s/NODENUM/${NODES}/g" -e "s/QUEUE/${QUEUE}/" > pbs_eye.sh
 chmod a+x pbs_eye.sh
 
