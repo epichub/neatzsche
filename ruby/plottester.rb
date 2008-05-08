2b1d5037413e7471680b36303b515eb974d54665
epic <epic@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2008-05-08 13:01:44 +0000

    jaja
    git-svn-id: https://svn.generation.no/neatzsche/trunk@309 7a382ede-a22f-0410-891f-d2f9ed330ce3

--- ruby/plottester.rb
+++ ruby/plottester.rb
@@ -18,7 +18,7 @@
 require 'plotter'
 
 @plotter = Plotter.new
-@plotter.init("/var/www/www.generation.no/htdocs/gores","epic",1,false)
+@plotter.init("generation.no","/home/www/www.generation.no/htdocs/gores","epic",1,false)
 @plotter.myloop(false)
 # @plotter.cgioutput()
 
