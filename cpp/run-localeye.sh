76bdca7e926fb7e8b7c98fe3d7561d6c5cee9998
wakko <wakko@7a382ede-a22f-0410-891f-d2f9ed330ce3>
2008-05-14 11:13:49 +0000

    teste
    git-svn-id: https://svn.generation.no/neatzsche/trunk@328 7a382ede-a22f-0410-891f-d2f9ed330ce3

---
+++ cpp/run-localeye.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+POPOP=$1
+let "ANODES=$2-1"
+NODES=$2
+GENERATIONS=$3
+RUNS=$4
+SETTINGS=$5
+QUEUE=$6
+INTER=$7
+
+echo $ANODES
+cat "templates/distconfig-eye.txt.template" | sed -e "s!POP!${POPOP}!g" -e "s/NODENUM/${ANODES}/g" -e "s/GENS/${GENERATIONS}/g" -e "s/RUNS/${RUNS}/g" -e "s!SETTINGFILE!${SETTINGS}!g" > "distconfig-eye.txt"
+cat "templates/pbs_eye.sh.template" | sed -e "s/NODENUM/${NODES}/g" -e "s/QUEUE/${QUEUE}/" > pbs_eye.sh
+chmod a+x pbs_eye.sh
+
+mpirun -hostfile hostfile distributor distconfig-eye.txt
